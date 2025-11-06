7a55c8c04fcf2dd287018abe4aaf10dbacc97095
FyndBo <fyndbo.m@gmail.com>
2025-11-06 23:11:54 +0100

    Tar bort loading temporärt för att testa build

--- app/loading.js
+++
@@ -1,9 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex items-center justify-center h-screen bg-black text-white animate-pulse">
-      <h1 className="text-4xl font-bold tracking-wide text-blue-400">
-        Loading Webneva...
-      </h1>
-    </div>
-  )
-}
