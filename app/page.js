c1dd74ebf4d63910fcdb421ade32d32a2467b991
FyndBo <fyndbo.m@gmail.com>
2025-11-06 22:58:42 +0100

    Flyttar app till rot och fixar vercel.json

--- app/page.js
+++ app/page.js
@@ -3,7 +3,7 @@ export default function Home() {
     <main className="flex flex-col items-center justify-center min-h-screen bg-black text-white text-center">
       <h1 className="text-4xl font-bold mb-4">Welcome to <span className="text-blue-400">Webneva</span></h1>
       <p className="text-lg text-gray-300">Your Next.js deployment is live 🎉</p>
-      <p className="mt-6 text-sm text-gray-500">Edit <code>src/app/page.js</code> to start building.</p>
+      <p className="mt-6 text-sm text-gray-500">Edit <code>src/app/page.js</code> to startt building.</p>
     </main>
   )
 }
